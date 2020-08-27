ee461708485961eac22f7bca7dfce2c76293ec16
(Benja Kunkel kunkelbenjamin08@gmail.com) <kunkelbenjamin08@gmail.com>
2020-08-27 19:41:56 -0300

    carrito

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 window.onload = function () {
   let contador = 1;
   let total = 35.99
+  let cosasEnElCarro= 2
   
   document.getElementById("+").onclick = function () {
       contador++;
       total+=35.99
+      cosasEnElCarro+=1
       document.getElementById("cantidad").innerHTML = contador
       document.getElementById("total1").innerHTML = "$" + Math.round(total)
+      document.getElementById("cantidades").innerHTML = cosasEnElCarro
+
 
 
   }
@@ -14,9 +18,12 @@ window.onload = function () {
       if (contador > 0) {
         contador--;
         total-=35.99
+        cosasEnElCarro-=1
       }
       document.getElementById("cantidad").innerHTML = contador
       document.getElementById("total1").innerHTML = "$" + Math.round(total)
+      document.getElementById("cantidades").innerHTML = cosasEnElCarro
+
 
   }
   
@@ -25,8 +32,11 @@ window.onload = function () {
   document.getElementById("++").onclick = function () {
       contador2++;
       total2+=35.99
+      cosasEnElCarro+=1
       document.getElementById("cantidad2").innerHTML = contador2
       document.getElementById("total2").innerHTML = "$" + Math.round(total2)
+      document.getElementById("cantidades").innerHTML = cosasEnElCarro
+
 
 
   }
@@ -34,9 +44,12 @@ window.onload = function () {
       if (contador2 > 0) {
         contador2--;
         total2-=35.99
+        cosasEnElCarro-=1
       }
       document.getElementById("cantidad2").innerHTML = contador2
       document.getElementById("total2").innerHTML = "$" + Math.round(total2)
-  }
+      document.getElementById("cantidades").innerHTML = cosasEnElCarro
 
+  }
+  
 }
