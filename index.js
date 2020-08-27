6618893519e257ad6e2ca012ff3b4bc6533fd988
(Benja Kunkel kunkelbenjamin08@gmail.com) <kunkelbenjamin08@gmail.com>
2020-08-27 19:10:16 -0300

    agregue la chance de sumar el parcial

--- index.js
+++ index.js
@@ -1,25 +1,42 @@
 window.onload = function () {
   let contador = 1;
+  let total = 35.99
+  
   document.getElementById("+").onclick = function () {
       contador++;
+      total+=35.99
       document.getElementById("cantidad").innerHTML = contador
+      document.getElementById("total1").innerHTML = "$" + Math.round(total)
+
+
   }
   document.getElementById("-").onclick = function () {
       if (contador > 0) {
         contador--;
+        total-=35.99
       }
       document.getElementById("cantidad").innerHTML = contador
+      document.getElementById("total1").innerHTML = "$" + Math.round(total)
+
   }
   
   let contador2 = 1;
+  let total2 = 35.99
   document.getElementById("++").onclick = function () {
       contador2++;
+      total2+=35.99
       document.getElementById("cantidad2").innerHTML = contador2
+      document.getElementById("total2").innerHTML = "$" + Math.round(total2)
+
+
   }
   document.getElementById("--").onclick = function () {
       if (contador2 > 0) {
         contador2--;
+        total2-=35.99
       }
       document.getElementById("cantidad2").innerHTML = contador2
+      document.getElementById("total2").innerHTML = "$" + Math.round(total2)
   }
+
 }
